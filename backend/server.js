106514fcd06ca9e1dd7d74a5a142b9bd99a2e8be
saalim829 <saalimm64@gmail.com>
2026-09-02 17:11:30 +0530

    API Working

---
+++ backend/server.js
@@ -0,0 +1,32 @@
+import express from 'express';
+import cors from 'cors';
+import 'dotenv/config';
+import http from 'http';
+import { error } from 'console';
+
+const PORT = process.env.PORT || 5000;
+const app = express();
+
+// MIDDLEWARES
+
+// DB
+
+// ROUTES
+app.get("/", (req,res) =>{
+    res.send("API WORKING");
+});
+
+const server = http.createServer(app);
+
+server.on("error", (error) => {
+    if(error.code === 'EADDRINUSE') {
+        console.error(`Port ${PORT} is already in use.`);
+        process.exit(l);
+    }
+
+    throw error;
+});
+
+server.listen(PORT, () => {
+    console.log(`Server Started on http://localhost:${PORT}`);
+})
